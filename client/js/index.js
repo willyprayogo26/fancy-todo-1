4fa04f13cc349432e360124e2107823a7f9ce82b
willyprayogo26 <willyprayogo26@gmail.com>
2019-04-08 12:38:27 +0700

    test google botton

--- client/js/index.js
+++ client/js/index.js
@@ -877,10 +877,15 @@ function onSignIn(googleUser) {
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
+    auth2.signOut()
+    .then(function () {
         console.log('User signed out.');
         localStorage.clear()
-    });
+    }
+    .catch(err => {
+        console.log(err)
+    })
+    );
 
     $("#user").hide()
     $("#todo-list").hide()
